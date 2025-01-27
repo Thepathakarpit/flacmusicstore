15b2f02aacf8c70c4a315896b057fa26e50e7249
Arpit Pathak <119810812+Thepathakarpit@users.noreply.github.com>
2025-01-27 10:06:25 +0530

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -20,14 +20,8 @@ async function searchTracks(query) {
         const data = await response.json();
         console.log('Received data:', data);
         
-        // Directly check if data is an array and has elements
-        if (Array.isArray(data) && data.length > 0) {
             displayResults(data);
             return data;
-        } else {
-            console.error('No results found or unknown error');
-            displayResults([]); // Clear previous results if no new results
-            return [];
         }
     } catch (error) {
         console.error('Error searching tracks:', error.message || error);
